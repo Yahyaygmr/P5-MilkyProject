923a87eaab3ea2c476d7ca23498b30b29821c89b
Yahyaygmr <yahyaygmr@gmail.com>
2024-05-27 15:16:31 +0300

    committed

--- MilkyProject.WebUI/Areas/Admin/Controllers/WhyUsController.cs
+++ MilkyProject.WebUI/Areas/Admin/Controllers/WhyUsController.cs
@@ -8,69 +8,30 @@ namespace MilkyProject.WebUI.Areas.Admin.Controllers
     [Route("Admin/{controller}/{action}/{id?}")]
     public class WhyUsController : Controller
     {
-        private readonly DynamicConsume<ResultWhyUsDto> _resultWhyUs;
-        private readonly DynamicConsume<UpdateWhyUsDto> _updateWhyUs;
-        private readonly DynamicConsume<CreateWhyUsDto> _createWhyUs;
-        private readonly DynamicConsume<object> _objectWhyUs;
+        private readonly DynamicConsume<UpdateWhyUsDto> _dynamicConsumeUpdateAbout;
 
-        public WhyUsController(DynamicConsume<ResultWhyUsDto> resultWhyUs, DynamicConsume<UpdateWhyUsDto> updateWhyUs, DynamicConsume<CreateWhyUsDto> createWhyUs, DynamicConsume<object> objectWhyUs)
+        public WhyUsController(DynamicConsume<UpdateWhyUsDto> dynamicConsumeUpdateAbout)
         {
-            _resultWhyUs = resultWhyUs;
-            _updateWhyUs = updateWhyUs;
-            _createWhyUs = createWhyUs;
-            _objectWhyUs = objectWhyUs;
+            _dynamicConsumeUpdateAbout = dynamicConsumeUpdateAbout;
         }
 
-        public async Task<IActionResult> Index()
-        {
-            var values = await _resultWhyUs.GetListAsync("WhyUss/GetWhyUs");
-            if (values != null)
-            {
-                return View(values);
-            }
-            return View();
-        }
         [HttpGet]
-        public IActionResult CreateWhyUs()
+        public async Task<IActionResult> Index(int id = 1)
         {
-            return View();
-        }
-        [HttpPost]
-        public async Task<IActionResult> CreateWhyUs(CreateWhyUsDto dto)
-        {
-            var result = await _createWhyUs.PostAsync("WhyUss", dto);
-            if (result > 0)
+            var value = await _dynamicConsumeUpdateAbout.GetByIdAsync("WhyUses/GetWhyUsById", id);
+            if (value != null)
             {
-                return RedirectToAction("Index");
-            }
-            return View(dto);
-        }
-        [HttpGet]
-        public async Task<IActionResult> UpdateWhyUs(int id)
-        {
-            var values = await _updateWhyUs.GetByIdAsync("WhyUss/GetWhyUsById", id);
-            if (values != null)
-            {
-                return View(values);
+                return View(value);
             }
             return View();
         }
         [HttpPost]
-        public async Task<IActionResult> UpdateWhyUs(UpdateWhyUsDto dto)
+        public async Task<IActionResult> Index(UpdateWhyUsDto dto)
         {
-            var result = await _updateWhyUs.PutAsync("WhyUss/UpdateWhyUs", dto);
+            int result = await _dynamicConsumeUpdateAbout.PutAsync("WhyUses/UpdateWhyUs", dto);
             if (result > 0)
             {
-                return RedirectToAction("Index");
-            }
-            return View(dto);
-        }
-        public async Task<IActionResult> DeleteWhyUs(int id)
-        {
-            var result = await _objectWhyUs.DeleteAsync("WhyUss/DeleteWhyUs", id);
-            if (result > 0)
-            {
-                return RedirectToAction("Index");
+                return View("Index");
             }
             return View();
         }
