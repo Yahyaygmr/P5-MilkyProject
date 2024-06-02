b5a0e8f7531b26acd46abbf620c6be43bffe2a16
Yahyaygmr <yahyaygmr@gmail.com>
2024-06-02 19:53:20 +0300

    created sending message ui side to admin

--- MilkyProject.WebUI/Controllers/ContactController.cs
+++ MilkyProject.WebUI/Controllers/ContactController.cs
@@ -1,14 +1,37 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using MilkyProject.DtoLayer.MessageDtos;
+using MilkyProject.EntityLayer.Concrete;
+using MilkyProject.WebUI.Models;
 
 namespace MilkyProject.WebUI.Controllers
 {
     [AllowAnonymous]
     public class ContactController : Controller
     {
+        private readonly DynamicConsume<CreateMessageDto> _dynamicConsumeCreateMessage;
+
+        public ContactController(DynamicConsume<CreateMessageDto> dynamicConsumeCreateMessage)
+        {
+            _dynamicConsumeCreateMessage = dynamicConsumeCreateMessage;
+        }
+
         public IActionResult Index()
         {
             return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> SendMessage(CreateMessageDto createMessageDto)
+        {
+            createMessageDto.Status = true;
+            createMessageDto.SendDate = DateTime.Now;
+
+            var result = await _dynamicConsumeCreateMessage.PostAsync("Messages", createMessageDto);
+            if(result>0)
+            {
+                return RedirectToAction("Index","Default");
+            }
+            return View(createMessageDto);
         }
     }
 }
